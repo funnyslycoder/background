d2847bfabbd773b1ee1c8fd1c5dc02acadaa6f95
Mayank singh <mayanksingh199025356001@gmail.com>
2021-10-21 12:54:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function qwe(){
+    var a=Math.floor(Math.random() * 2);
+    console.log(a);
+    if (a==0){
+        document.body.style.backgroundColor = "blue";
+    }
+    if (a==1){
+        document.body.style.backgroundColor = "yellow";
+    }
+}
